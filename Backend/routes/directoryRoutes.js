29ff619d5a78c7cb33d529a821d7342b353aabad
raqibreyaz <raquibreyaz111@gmail.com>
2026-01-19 01:23:13 +0530

    migrating to VFS, files part done

--- Backend/routes/directoryRoutes.js
+++ Backend/routes/directoryRoutes.js
@@ -1,49 +1,95 @@
 import express from "express";
-import fs, { readdir } from "fs/promises";
-import path from "node:path";
+import crypto from "node:crypto";
+import dirsDB from "../dirsDB.json" with { type: "json" };
+import filesDB from "../filesDB.json" with { type: "json" };
+import { writeFile } from "node:fs/promises";
 
 const router = express.Router();
 
 // serving directory contents
-router.get("/{*path}", async (req, res) => {
-  const dirpath = req.params.path ?? [];
+router.get("/{:dir_id}", async (req, res) => {
+  const { dir_id } = req.params;
 
-  const full_dir_path = path.join(
-    process.cwd(),
-    "storage/",
-    path.join("/", ...dirpath),
+  const dir = dir_id ? dirsDB.find((dir) => dir.id === dir_id) : dirsDB[0];
+
+  if (!dir) return res.json({ message: "directory not found!" });
+
+  const files = filesDB.filter((file) => dir.files.includes(file.id));
+
+  const directories = dirsDB.filter((sub_dir) =>
+    dir.directories.includes(sub_dir.id),
   );
 
-  console.log(full_dir_path);
-
-  const files = await readdir(full_dir_path, { withFileTypes: true }).then(
-    (dirents) =>
-      dirents
-        .filter((dirent) => dirent.isDirectory() || dirent.isFile())
-        .map((dirent) => ({
-          is_directory: dirent.isDirectory(),
-          name: dirent.name,
-          parent_path: dirent.parentPath,
-        })),
-  );
-  res.header("Content-Type", "application/json");
-  return res.status(200).json(files);
+  res.json({ ...dir, files, directories });
 });
 
 // creating directory
-router.post("/*path", async (req, res) => {
-  const dirpath = req.params.path;
-  const full_dir_path = path.join(
-    process.cwd(),
-    "storage/",
-    path.join("/", ...dirpath),
-  );
+router.post("/:dirname", async (req, res) => {
+  const dirname = req.params.dirname;
+  const dir_id = crypto.randomUUID();
+  const parent_dir_id = req.headers.parent_dir_id;
 
-  console.log(full_dir_path);
+  // add entry of this directory in dirsDB array
+  dirsDB.push({
+    id: dir_id,
+    name: dirname,
+    parentDir: parent_dir_id,
+    directories: [],
+    files: [],
+  });
 
-  await fs.mkdir(full_dir_path);
+  // add entry of this directory to its parent also
+  dirsDB.forEach((dir) => {
+    if (dir.id === parent_dir_id) dir.directories.push(dir_id);
+  });
 
-  res.json({ message: "Directory Created" });
+  // update dirsDB file
+  await writeFile(`${process.cwd()}/dirsDB.json`, JSON.stringify(dirsDB));
+
+  res.json({ message: "Directory created!" });
+});
+
+// changing directory name
+router.patch("/:dir_id", async (req, res) => {
+  if (!req.body.new_dirname)
+    return res.json({ message: "New Dirname required!" });
+
+  const dir_id = req.params.dir_id;
+
+  const new_dirname = req.body.new_dirname;
+
+  dirsDB.forEach((dir) => {
+    if (dir.id === dir_id) dir.name = new_dirname;
+  });
+
+  await writeFile(`${process.cwd()}/dirsDB.json`, JSON.stringify(dirsDB));
+
+  res.json({ message: "Directory name updated!" });
+});
+
+// deleting directory
+router.delete("/:dir_id", async (req, res) => {
+  // const dir_id = req.params.dir_id;
+  // const curr_dir = dirsDB.find((dir) => dir.id === dir_id);
+
+  // // remove entry of curr dir from its parent
+  // dirsDB.forEach((dir) => {
+  //   if (dir.id === curr_dir.parentDir)
+  //     dir.directories = dir.directories.filter((d_id) => dir_id != dir_id);
+  // });
+
+  // // remove curr dir from dirsDB array
+  // const dir_ind = dirsDB.findIndex((dir) => dir.id === dir_id);
+  // dirsDB.splice(dir_ind, 1);
+
+  // // remove all the files of the directory
+
+  // // remove all the sub directories of the directory
+
+  // // update the dirsDB file
+  // await writeFile(`${process.cwd()}/dirsDB.json`, JSON.stringify(dirsDB));
+
+  res.json({ message: "TODO!" });
 });
 
 export default router;
--- Backend/routes/fileRoutes.js
+++ Backend/routes/fileRoutes.js
@@ -2,18 +2,22 @@ import express from "express";
 import fs from "fs/promises";
 import { createWriteStream } from "fs";
 import path from "node:path";
+import crypto from "crypto";
+import { stat, writeFile } from "node:fs/promises";
+import filesDB from "../filesDB.json" with { type: "json" };
+import dirsDB from "../dirsDB.json" with { type: "json" };
 
 const router = express.Router();
 
 // serving file contents
-router.get("/*path", (req, res) => {
-  const filepath = req.params.path ?? [];
+router.get("/:file_id", (req, res) => {
+  const file_id = req.params.file_id;
 
-  const fullpath = path.join(
-    process.cwd(),
-    "storage/",
-    path.join("/", ...filepath),
-  );
+  const file = filesDB.find((file) => file.id === file_id);
+
+  if (!file) return res.status(404).json({ message: "file not found!" });
+
+  const fullpath = path.join(process.cwd(), "storage/", file_id + file.extname);
 
   if (req.query.action === "download")
     res.header("Content-Disposition", `attachment;`);
@@ -22,53 +26,105 @@ router.get("/*path", (req, res) => {
 });
 
 // file upload
-router.post("/*path", (req, res) => {
-  const filepath = req.params.path;
-  const fullpath = path.join(
-    process.cwd(),
-    "storage/",
-    path.join("/", ...filepath),
-  );
-  const write_stream = createWriteStream(fullpath);
+router.post("/:filename", async (req, res) => {
+  const filepath = req.params.filename;
+  const parent_dir_id = req.headers.parent_dir_id ?? dirsDB[0].id;
+  const file_id = crypto.randomUUID();
+  const file_ext = path.extname(filepath);
 
+  // create a file in the storage directory
+  // assign name as the unique id
+  const fullpath = path.join(process.cwd(), "storage/", file_id + file_ext);
+
+  // store the contents of the file
+  const write_stream = createWriteStream(fullpath);
   req.pipe(write_stream);
 
-  res.status(201).json({ message: "Got the File!" });
+  req.on("end", async () => {
+    const { size } = await stat(fullpath);
+
+    // add entry in filesDB
+    filesDB.push({
+      id: file_id,
+      name: filepath,
+      size,
+      parentDir: parent_dir_id,
+      extname: file_ext,
+    });
+
+    // add entry in dirsDB
+    dirsDB.forEach((dir) => {
+      if (dir.id === parent_dir_id) dir.files.push(file_id);
+    });
+
+    // update the filesDB file
+    await writeFile(
+      path.join(process.cwd(), "filesDB.json"),
+      JSON.stringify(filesDB),
+    );
+    
+    // update the dirsDB file
+    await writeFile(
+      path.join(process.cwd(), "dirsDB.json"),
+      JSON.stringify(dirsDB),
+    );
+    
+    res.status(201).json({ message: "Got the File!" });
+  });
+
+  req.on("error", (err) => res.status(400).json({ message: err.message }));
 });
 
 // file renaming
-router.patch("/*path", async (req, res) => {
+router.patch("/:file_id", async (req, res) => {
   if (!req.body.new_filename)
     return res.status(400).json({ message: "new filename required!" });
 
-  let filepath = req.params.path ?? [];
-  console.log(filepath);
+  const file_id = req.params.file_id;
 
-  const old_filename = path.join(
-    process.cwd(),
-    "storage/",
-    path.join("/", ...filepath),
-  );
-  const new_filename = path.join(
-    process.cwd(),
-    "storage",
-    path.join("/", ...filepath.slice(0, -1)),
-    path.join("/", req.body.new_filename),
+  const file = filesDB.find((file) => file.id === file_id);
+  if (!file) return res.status(404).json({ message: "file not found" });
+
+  file.name = req.body.new_filename;
+
+  await writeFile(
+    path.join(process.cwd(), "filesDB.json"),
+    JSON.stringify(filesDB),
   );
 
-  await fs.rename(old_filename, new_filename);
   res.status(200).json({ message: "File Renamed!" });
 });
 
 // file deletion
-router.delete("/*path", async (req, res) => {
-  const filepath = req.params.path ?? [];
-  const fullpath = path.join(
-    process.cwd(),
-    "storage/",
-    path.join("/", ...filepath),
-  );
+router.delete("/:file_id", async (req, res) => {
+  const file_id = req.params.file_id;
+
+  // find the file
+  const file = filesDB.find((file) => file.id === file_id);
+  if (!file) return res.status(404).json({ message: "file not found" });
+
+  const fullpath = path.join(process.cwd(), "storage/", file.id + file.extname);
   await fs.rm(fullpath, { recursive: true, force: true });
+
+  // remove from filesDB array
+  const file_ind = filesDB.findIndex((file) => file.id === file_id);
+  filesDB.splice(file_ind, 1);
+
+  // remove from dirsDB array
+  const parent_dir = dirsDB.find((dir) => dir.id === file.parentDir);
+  parent_dir.files = parent_dir.files.filter((file_id) => file_id !== file.id);
+
+  // update the filesDB file
+  await writeFile(
+    path.join(process.cwd(), "filesDB.json"),
+    JSON.stringify(filesDB),
+  );
+
+  // update the dirsDB file
+  await writeFile(
+    path.join(process.cwd(), "dirsDB.json"),
+    JSON.stringify(dirsDB),
+  );
 
   res.status(200).json({ message: "File Deleted!" });
 });
