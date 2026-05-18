cf2211e06e3013aa3069fc89c5c08feff668d8c6
Raquib Reyaz <raquibreyaz111@gmail.com>
2026-05-19 01:59:54 +0530

    feat: prevent duplicate webhook processing using Redis and delivery ID tracking

--- Backend/src/controllers/deploy.controller.js
+++ Backend/src/controllers/deploy.controller.js
@@ -1,13 +1,11 @@
 import { spawn } from "node:child_process";
+import redisClient from "../config/redis.js";
 import verifyGithubWebhookSignature from "../helpers/verifyGithubWebhookSignature.js";
 
-/*
-TODO
-1. avoid duplicate events via uniqueness
-*/
 export const githubWebhook = async (req, res) => {
   const signature = req.headers["x-hub-signature-256"];
   const eventType = req.headers["x-github-event"];
+  const deliveryId = req.headers["x-github-delivery"];
 
   console.log("verifying signature...");
   // reject malformed event
@@ -21,6 +19,16 @@ export const githubWebhook = async (req, res) => {
     console.log("non push event received!");
     return res.sendStatus(200);
   }
+
+  // return error for bad delivery id
+  if (!deliveryId) return res.sendStatus(400);
+
+  // ignore if the event already processed
+  const inserted = await redisClient.set(`github:webhook:${deliveryId}`, "1", {
+    expiration: { type: "EX", value: 60 * 15 },
+    condition: "NX",
+  });
+  if (inserted === null) return res.sendStatus(200);
 
   const payload = JSON.parse(req.body.toString("utf-8"));
 
@@ -44,14 +52,18 @@ export const githubWebhook = async (req, res) => {
 
   // do 'pnpm install' when package.json changed
   console.log("checking new packages installation requirement...");
-  const shouldInstall =
-    changedFiles.some((f) => f === "Backend/package.json") ||
-    changedFiles.some((f) => f === "Backend/pnpm-lock.yaml");
+  const shouldInstall = changedFiles.some((f) => {
+    const name = f.toLowerCase();
+    return name === "backend/package.json" || name === "backend/pnpm-lock.yaml";
+  });
 
   if (!shouldInstall) console.log("no new package installation required!!");
 
   // sending early ACK to github
   res.sendStatus(200);
+
+  // if (Object.entries(deliveryIds).length >= 5) deliveryIds = {};
+  // deliveryIds[deliveryId] = true;
 
   const scriptPath =
     "/home/ubuntu/Personal-Google-Drive/Backend/scripts/deploy-backend.sh";
